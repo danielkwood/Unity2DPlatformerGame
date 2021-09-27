d0b615796849a8763a8f196d4b859af7b7a9aded
danielkwood <danielkwood@users.noreply.github.com>
2021-09-27 22:58:19 +1000

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float offset;
+    public float offsetSmoothing;
+    private Vector3 playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+
+        if(player.transform.localScale.x > 0f)
+        {
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+    }
+}
